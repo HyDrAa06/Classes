0541fc6e40dd5bcf2d2b3a62c6a1e4fbc286ebb4
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-10-10 11:42:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = int.Parse(Console.ReadLine());
+
+            var person = new Class1();
+
+            person.Age = age;
+            person.Name = name;
+
+            Console.WriteLine(person.Name + ", " + person.Age);
+        }
+    }
+}
