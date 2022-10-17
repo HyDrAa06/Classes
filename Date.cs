8907bb6dee6cd2959b01fe0160bd865148e617e6
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-10-17 17:33:57 +0300

    Add files via upload

---
+++ Date.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    internal class Date
+    {
+        private int year;
+        private int month;
+        private int day;
+        private int totalDays;
+
+        public Date(int year, int month, int day,int totalDays)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.totalDays = totalDays;
+        }
+
+        public int Dating()
+        {
+            totalDays = month + ((year - 1) * 12);
+            totalDays = day + ((totalDays - 1) * 31);
+            return totalDays;
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Date:");
+            int[]firstDate=Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            Console.WriteLine("Second Date:");
+            int[] secondDate = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int totalday1 = 0;
+            int totalday2 = 0;
+
+            Date date = new Date(firstDate[0], firstDate[1], firstDate[2], totalday1);
+            Date date2 = new Date(secondDate[0], secondDate[1], secondDate[2], totalday2);
+
+            int total = date.Dating();
+            int total2 =date2.Dating();
+
+            if(total2 < total)
+            {
+                Console.WriteLine("Difference: " + (total - total2));
+            }
+            else
+            {
+                Console.WriteLine("Difference: " + (total2 - total));
+            }
+            
+            
+        }
+    }
+}
