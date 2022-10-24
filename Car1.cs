eb48c3a2b03a33b6928c90de82bb1ea95cf0e036
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-10-24 20:55:55 +0300

    Add files via upload

---
+++ Car1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    internal class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption;
+        private double travelledDistance;
+        private double kmAmount;
+
+
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {            
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumption = fuelConsumption;
+            TravelledDistance = 0;
+            KmAmount = 0;
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public double KmAmount
+        {
+            get { return kmAmount; }
+            set { kmAmount = value; }
+        }
+
+        public void Drive()
+        {
+            if(fuelAmount >= fuelConsumption * kmAmount)
+            {
+                fuelAmount = fuelAmount - fuelConsumption*kmAmount;
+                travelledDistance += kmAmount;
+            }
+
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"{model} {fuelAmount} {travelledDistance}");
+        }
+
+    }
+}
---
+++ Program6.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SpeedRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cars:");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Car info:");
+                string[] carInfo = Console.ReadLine().Split(" ").ToArray();
+
+                Car car = new Car(carInfo[0], double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+                cars.Add(car);
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("Command:");
+                string[] command = Console.ReadLine().Split(" ").ToArray();
+
+                if (command[0] == "End")
+                {
+                    break;
+                }
+
+                double amountOfKm = double.Parse(command[2]);
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (cars[i].Model == command[1])
+                    {
+                        cars[i].KmAmount = amountOfKm;
+                        cars[i].Drive();
+                    }
+                }
+            }
+
+            Console.WriteLine("Info for cars:");
+
+            for (int i = 0; i < n; i++)
+            {
+                cars[i].Output();
+            }
+
+        }
+    }
+}
